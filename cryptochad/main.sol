fa2236992be354ecc62ba4dc83c09bf6fc5df0d5
Clément <86530475+clementciron@users.noreply.github.com>
2022-09-30 23:02:30 +0200

    Update main.sol

--- cryptochad/main.sol
+++ cryptochad/main.sol
@@ -8,7 +8,7 @@ contract Campaign_contract {
     struct Request {
         string description;
         uint value;
-        address recipient;
+        address payable recipient;
         bool complete;
         uint approvalCount; //votes yes
         mapping(address => bool) approvals;
@@ -17,10 +17,11 @@ contract Campaign_contract {
     uint numRequests; //
     mapping (uint => Request) requests;
 
-    Request[] public requests;
+   
     address public manager;
     uint public minimumContribution;
-   mapping(address => bool) public approvers;
+    uint public approversCount;
+    mapping(address => bool) public approvers;
 
     modifier restricted(){
         require(msg.sender == manager);
@@ -36,9 +37,10 @@ contract Campaign_contract {
         require(msg.value > minimumContribution);
 
         approvers[msg.sender] = true; //ajoute une nouvelle key 
+        approversCount++;
     }
 
-    function createRequest(string calldata description, uint value, address recipient) public restricted {
+    function createRequest(string calldata description, uint value, address payable recipient) public restricted {
         require(approvers[msg.sender]);
         Request storage r = requests[numRequests++]; //modifié car mapping dans struct n'est plus possible
         r.description = description;
@@ -50,11 +52,24 @@ contract Campaign_contract {
     }
 
     function approveRequest(uint index) public{
-        require(approvers[msg.sender]);
-        require(!requests[index].approvals[msg.sender]); //si la personne a déjà voté et que son addresse est dans le mapping = false
+        Request storage r = requests[index];
 
-        requests[index].approvals[msg.sender] = true;
-        requests[index].approvalCount++;
+        require(approvers[msg.sender]);
+        require(!r.approvals[msg.sender]); //si la personne a déjà voté et que son addresse est dans le mapping = false
+
+        r.approvals[msg.sender] = true; //requests[index] est remplacé par r
+        r.approvalCount++;
     }
+    function finalizeRequest(uint index) public restricted{
+        Request storage r = requests[index];
+        
+        require(r.approvalCount > (approversCount / 2));
+        require(!r.complete);
+        
+        r.recipient.transfer(r.value);
+        r.complete = true;
+
+    }
+  
 
 }
