92dbaaf82ef00df9c3f448527dd6c780f014d9ee
Clément <86530475+clementciron@users.noreply.github.com>
2022-09-30 17:58:58 +0200

    Update main.sol

--- cryptochad/main.sol
+++ cryptochad/main.sol
@@ -10,12 +10,17 @@ contract Campaign_contract {
         uint value;
         address recipient;
         bool complete;
+        uint approvalCount; //votes yes
+        mapping(address => bool) approvals;
     }
+
+    uint numRequests; //
+    mapping (uint => Request) requests;
 
     Request[] public requests;
     address public manager;
     uint public minimumContribution;
-    address[] public approvers;
+   mapping(address => bool) public approvers;
 
     modifier restricted(){
         require(msg.sender == manager);
@@ -30,7 +35,26 @@ contract Campaign_contract {
     function contribute() public payable {
         require(msg.value > minimumContribution);
 
-        approvers.push(msg.sender);
+        approvers[msg.sender] = true; //ajoute une nouvelle key 
+    }
+
+    function createRequest(string calldata description, uint value, address recipient) public restricted {
+        require(approvers[msg.sender]);
+        Request storage r = requests[numRequests++]; //modifié car mapping dans struct n'est plus possible
+        r.description = description;
+        r.value = value;
+        r.recipient = recipient;
+        r.complete = false;
+        r.approvalCount = 0;
+        
+    }
+
+    function approveRequest(uint index) public{
+        require(approvers[msg.sender]);
+        require(!requests[index].approvals[msg.sender]); //si la personne a déjà voté et que son addresse est dans le mapping = false
+
+        requests[index].approvals[msg.sender] = true;
+        requests[index].approvalCount++;
     }
 
 }
