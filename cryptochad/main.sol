1c682e6f155d660b9a5202f1af61c465399e40cf
Clément <86530475+clementciron@users.noreply.github.com>
2022-10-02 16:11:12 +0200

    Update main.sol

--- cryptochad/main.sol
+++ cryptochad/main.sol
@@ -2,33 +2,47 @@
 
 pragma solidity 0.8.15;
 
+//le paiement sera fait par l'user qui demande le déploiement d'un nouveau contrat
+//campaignFacotry permet aussi de sécuriser le contrat en évitant de le modifier
+contract CampaignFactory {
+    address[] public deployedCampaigns;
 
+    function createCampaign(uint256 minimum) public {
+        address newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(newCampaign);
+    }
+
+    //donne la liste des contrats déployés
+    function getDeployedCampaigns() public view returns (address[]){
+        
+    }
+}
 
 contract Campaign_contract {
     struct Request {
         string description;
-        uint value;
+        uint256 value;
         address payable recipient;
         bool complete;
-        uint approvalCount; //votes yes
+        uint256 approvalCount; //votes yes
         mapping(address => bool) approvals;
     }
 
-    uint numRequests; //
-    mapping (uint => Request) requests;
+    uint256 numRequests; //
+    mapping(uint256 => Request) requests;
 
-   
     address public manager;
-    uint public minimumContribution;
-    uint public approversCount;
+    uint256 public minimumContribution;
+    uint256 public approversCount;
     mapping(address => bool) public approvers;
 
-    modifier restricted(){
+    //permet d'executer la fonction que si l'addresse est celle du manager
+    modifier restricted() {
         require(msg.sender == manager);
         _;
     }
 
-    function Campaign(uint minimum) public{
+    function Campaign(uint256 minimum, address creator) public {
         manager = msg.sender;
         minimumContribution = minimum;
     }
@@ -36,11 +50,15 @@ contract Campaign_contract {
     function contribute() public payable {
         require(msg.value > minimumContribution);
 
-        approvers[msg.sender] = true; //ajoute une nouvelle key 
+        approvers[msg.sender] = true; //ajoute une nouvelle key
         approversCount++;
     }
 
-    function createRequest(string calldata description, uint value, address payable recipient) public restricted {
+    function createRequest(
+        string calldata description,
+        uint256 value,
+        address payable recipient
+    ) public restricted {
         require(approvers[msg.sender]);
         Request storage r = requests[numRequests++]; //modifié car mapping dans struct n'est plus possible
         r.description = description;
@@ -48,10 +66,9 @@ contract Campaign_contract {
         r.recipient = recipient;
         r.complete = false;
         r.approvalCount = 0;
-        
     }
 
-    function approveRequest(uint index) public{
+    function approveRequest(uint256 index) public {
         Request storage r = requests[index];
 
         require(approvers[msg.sender]);
@@ -60,16 +77,14 @@ contract Campaign_contract {
         r.approvals[msg.sender] = true; //requests[index] est remplacé par r
         r.approvalCount++;
     }
-    function finalizeRequest(uint index) public restricted{
+
+    function finalizeRequest(uint256 index) public restricted {
         Request storage r = requests[index];
-        
+
         require(r.approvalCount > (approversCount / 2));
         require(!r.complete);
-        
+
         r.recipient.transfer(r.value);
         r.complete = true;
-
     }
-  
-
 }
